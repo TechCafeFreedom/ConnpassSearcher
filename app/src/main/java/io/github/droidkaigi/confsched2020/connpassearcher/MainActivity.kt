e9dfbe696d16908cc34419d337d0a87f28f69cab
Daishin16 <sakuya1202@icloud.com>
2020-02-28 13:24:28 +0900

    first commit

---
+++ app/src/main/java/io/github/droidkaigi/confsched2020/connpassearcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.droidkaigi.confsched2020.connpassearcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
